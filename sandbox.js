a96368bcdbbc90a975390ed747bb15b659790898
rdxdheeraj75 <dheerajp748@gmail.com>
2023-05-11 18:15:16 +0530

    Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,14 @@
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(area('hello'));
+console.log(circ(7.5));
