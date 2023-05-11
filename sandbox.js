088618c97bc153bb173660417e724a9741a3d87c
rdxdheeraj75 <dheerajp748@gmail.com>
2023-05-12 01:06:49 +0530

    Dynamic Types

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,17 @@
-// arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
-var numbers = [10, 20, 12, 15];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-// objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: ['running'],
-};
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+var ninja;
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,23 @@
-// arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let age: any = 25;
 
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
 
-let numbers = [10, 20, 12, 15];
+let mixed: any[] = [];
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
+let ninja: { name: any, age: any };
 
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
 
-// objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
-};
-
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-
-ninja = {
-  name: 'yoshi',
-  belt: 'orange',
-  age: 40,
-  // skills: ['running'],
-};
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
